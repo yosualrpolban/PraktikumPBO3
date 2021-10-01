9aadb5862fd0d8994fc28cbf1e55a4c5fcccc773
yosualrpolban <91033585+yosualrpolban@users.noreply.github.com>
2021-10-01 07:43:19 +0700

    Update Soal4.java

--- src/Soal4.java
+++ src/Soal4.java
@@ -7,17 +7,21 @@ public class Soal4 {
 		Scanner userInput = new Scanner(System.in);
 		int gajiPokok = 500000;
 		int items = userInput.nextInt();
-		int itemsPrice = items * 50000;	
+		int itemsPrice = 0;	
 		if((items >= 40) && (items<80)) {
+			itemsPrice = items * 50000;
 			int gaji = (int) (gajiPokok + (itemsPrice * 0.25));
 			System.out.println(gaji);
 		}else if(items >= 80) {
+			itemsPrice = items * 50000;
 			int gaji = (int) (gajiPokok + (itemsPrice * 0.35));
 			System.out.println(gaji);
 		}else if(items<=15) {
+			itemsPrice = (15-items) * 50000;
 			int gaji = (int) (gajiPokok - (itemsPrice * 0.15));
 			System.out.println(gaji);
 		}else {
+			itemsPrice = items * 50000;
 			int gaji = (int) (gajiPokok + (itemsPrice * 0.1));
 			System.out.println(gaji);
 		}
